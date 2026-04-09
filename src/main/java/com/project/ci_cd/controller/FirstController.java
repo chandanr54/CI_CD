0335092e845bafeafb4068e171e4fa4292ff39d4
chandanr54 <67356198+chandanr54@users.noreply.github.com>
2026-04-09 11:31:41 +0530

    Initial commit

---
+++ src/main/java/com/project/ci_cd/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.project.ci_cd.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class FirstController {
+
+    @GetMapping
+    public String first() {
+        return "Hello World";
+    }
+}
